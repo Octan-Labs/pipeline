40d0218dffc11fbb8b65c33a28d2f0b86138759c
Truong Ma Phi <trmaphi@gmail.com>
2023-04-08 22:23:20 +0700

    chore: first working version

---
+++ indexer/schemas/iceberg/transactions.sql
@@ -0,0 +1,18 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS demo.bsc.transactions (
+    hash STRING,
+    nonce LONG,
+    block_hash STRING,
+    block_number LONG,
+    block_timestamp TIMESTAMP,
+    transaction_index LONG,
+    from_address STRING,
+    to_address STRING,
+    value DECIMAL(38,0),
+    gas LONG,
+    gas_price LONG,
+    input STRING,
+    max_fee_per_gas LONG,
+    max_priority_fee_per_gas LONG,
+    transaction_type LONG
+)
+PARTITIONED BY (truncate(1000, block_number), days(block_timestamp), from_address, to_address, transaction_type)
