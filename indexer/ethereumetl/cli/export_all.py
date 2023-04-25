d9108f3960eccb74dd647146b9711e62047149c2
tuannm <nminh.tuan106@gmail.com>
2023-04-25 13:57:14 +0700

    Support env for export cli

--- indexer/ethereumetl/cli/export_all.py
+++ indexer/ethereumetl/cli/export_all.py
@@ -105,82 +105,20 @@ def get_partitions(start, end, partition_batch_size, provider_uri):
         raise ValueError('start and end must be either block numbers or ISO dates or Unix times')
 
 
-@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.option(
-    "-s",
-    "--start",
-    required=True,
-    type=str,
-    default=lambda: environ.get("START_BLOCK"),
-    help="Start block/ISO date/Unix time",
-)
-@click.option(
-    "-e",
-    "--end",
-    required=True,
-    type=str,
-    default=lambda: environ.get("END_BLOCK"),
-    help="End block/ISO date/Unix time",
-)
-@click.option(
-    "-b",
-    default=10000,
-    show_default=True,
-    type=int,
-    default=lambda: environ.get("PARTITION_BATCH_SIZE"),
-    help="The number of blocks to export in partition.",
-)
-@click.option(
-    "-p",
-    "--provider-uri",
-    show_default=True,
-    type=str,
-    default=lambda: environ.get("PROVIDER_URI"),
-    help="The URI of the web3 provider e.g. "
-    "file://$HOME/Library/Ethereum/geth.ipc or https://mainnet.infura.io",
-)
-@click.option(
-    "-o",
-    "--output-dir",
-    show_default=True,
-    type=str,
-    default=lambda: environ.get("OUTPUT_DIR"),
-    help="Output directory, partitioned in Hive style.",
-)
-@click.option(
-    "-w",
-    "--max-workers",
-    show_default=True,
-    type=int,
-    default=lambda: environ.get("MAX_WORKERS"),
-    help="The maximum number of workers.",
-)
-@click.option(
-    "-B",
-    "--export-batch-size",
-    show_default=True,
-    type=int,
-    default=lambda: environ.get("EXPORT_BATCH_SIZE"),
-    help="The number of requests in JSON RPC batches.",
-)
-@click.option(
-    "-c",
-    "--chain",
-    default="ethereum",
-    show_default=True,
-    type=str,
-    help="The chain network to connect to.",
-)
-def export_all(
-    start,
-    end,
-    provider_uri,
-    output_dir,
-    partition_batch_size=10000,
-    max_workers=5,
-    export_batch_size=100,
-    chain="ethereum",
-):
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option('-s', '--start', required=True, type=str, default=lambda: environ.get("START_BLOCK"), help='Start block/ISO date/Unix time')
+@click.option('-e', '--end', required=True, type=str, default=lambda: environ.get("END_BLOCK"), help='End block/ISO date/Unix time')
+@click.option('-b', '--partition-batch-size', default=lambda: environ.get("PARTITION_BATCH_SIZE", 10000), show_default=True, type=int,
+              help='The number of blocks to export in partition.')
+@click.option('-p', '--provider-uri', default=lambda: environ.get("PROVIDER_URI", 'https://mainnet.infura.io'), show_default=True, type=str,
+              help='The URI of the web3 provider e.g. '
+                   'file://$HOME/Library/Ethereum/geth.ipc or https://mainnet.infura.io')
+@click.option('-o', '--output-dir', default=lambda: environ.get("OUTPUT_DIR", 'output'), show_default=True, type=str, help='Output directory, partitioned in Hive style.')
+@click.option('-w', '--max-workers', default=lambda: environ.get("MAX_WORKERS", 5), show_default=True, type=int, help='The maximum number of workers.')
+@click.option('-B', '--export-batch-size', default=lambda: environ.get("EXPORT_BATCH_SIZE", 100), show_default=True, type=int, help='The number of requests in JSON RPC batches.')
+@click.option('-c', '--chain', default='ethereum', show_default=True, type=str, help='The chain network to connect to.')
+def export_all(start, end, partition_batch_size, provider_uri, output_dir, max_workers, export_batch_size,
+               chain='ethereum'):
     """Exports all data for a range of blocks."""
     provider_uri = check_classic_provider_uri(chain, provider_uri)
     export_all_common(get_partitions(start, end, partition_batch_size, provider_uri),
