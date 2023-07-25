9f23082cce8fedd1df16e5b7655cd805946b6370
tuannm <nminh.tuan106@gmail.com>
2023-07-25 15:40:03 +0700

    update: dockerize rename partition task

--- dags/merge_hour_partition.py
+++ dags/merge_hour_partition.py
@@ -1,90 +1,8 @@
 from datetime import datetime, timedelta
-import boto3
-import re
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from kubernetes.client import models as k8s
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.kubernetes.secret import Secret
-
-class EntityType:
-    BLOCK = 'block'
-    TRANSACTION = 'transaction'
-    LOG = 'log'
-    TOKEN_TRANSFER = 'token_transfer'
-    TRACE = 'trace'
-    CONTRACT = 'contract'
-    TOKEN = 'token'
-
-
-def execute(bucket_name, bucket, entity, date, sub_path):
-    block_numbers = []
-    for obj in bucket.objects.filter(Prefix="{sub_path}{entity}s/date={date}/hour".format(entity=entity, date=date, sub_path=sub_path)):
-        block_numbers.append(re.findall(r'\d+', obj.key.split("/")[-1]))
-        obj.delete()
-
-    print(block_numbers)
-
-    start_block = min((min(block_number)
-                      for block_number in block_numbers), default="")
-    end_block = max((max(block_number)
-                    for block_number in block_numbers), default="")
-    file_name = "{entity}s_{start_block}_{end_block}.parquet".format(
-        entity=entity, start_block=start_block, end_block=end_block, sub_path=sub_path)
-
-    for obj in bucket.objects.filter(Prefix="{sub_path}{entity}s/date={date}/repartition".format(entity=entity, date=date, sub_path=sub_path)):
-        if ("/part" not in obj.key):
-            obj.delete()
-        else:
-            print(obj.key)
-            s3.Object(bucket_name, '{sub_path}{entity}s/date={date}/{file_name}'.format(entity=entity, file_name=file_name,
-                      date=date, sub_path=sub_path)).copy_from(CopySource='{bucket_name}/{key}'.format(bucket_name=bucket_name, key=obj.key))
-            s3.Object(bucket_name, obj.key).delete()
-
-
-def main(**kwargs):
-    conf = kwargs['dag_run'].conf
-    path = conf['base_path'].split("/")
-    bucket_name = path[0]
-    sub_path = "/".join(path[1:]) + "/" if len(path) > 1 else ""
-    entities = conf['entities']
-    date = conf['date']
-    
-    aws_access_key_id=Secret(deploy_type='env',
-                             deploy_target='AWS_ACCESS_KEY_ID',
-                             secret='indexer-aws-key',
-                             key='aws_access_key_id')
-    aws_secret_access_key=Secret(deploy_type='env',
-                                 deploy_target='AWS_SECRET_ACCESS_KEY',
-                                 secret='indexer-aws-key',
-                                 key='aws_secret_access_key')
-    
-    # Create connection to S3
-    global s3
-    s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-
-    # Get bucket object
-    bucket = s3.Bucket(bucket_name)
-
-    if EntityType.BLOCK in entities:
-        execute(bucket_name, bucket, EntityType.BLOCK, date, sub_path)
-
-    if EntityType.TRANSACTION in entities:
-        execute(bucket_name, bucket, EntityType.TRANSACTION, date, sub_path)
-
-    if EntityType.LOG in entities:
-        execute(bucket_name, bucket, EntityType.LOG, date, sub_path)
-
-    if EntityType.TOKEN_TRANSFER in entities:
-        execute(bucket_name, bucket, EntityType.TOKEN_TRANSFER, date, sub_path)
-
-    if EntityType.TRACE in entities:
-        execute(bucket_name, bucket, EntityType.TRACE, date, sub_path)
-
-    if EntityType.CONTRACT in entities:
-        execute(bucket_name, bucket, EntityType.CONTRACT, date, sub_path)
-
-    if EntityType.TOKEN in entities:
-        execute(bucket_name, bucket, EntityType.TOKEN, date, sub_path)
 
 default_args = {
     'owner': 'airflow',
@@ -171,10 +89,37 @@ merge_files = KubernetesPodOperator(
             dag=dag,
         )
 
-rename_and_delete_file = PythonOperator(
-    task_id='rename_and_delete_file',
-    python_callable=main,
-    dag=dag
-)
+env_vars = [
+    k8s.V1EnvVar(name='BASE_PATH', value="{{ dag_run.conf['base_path'] }}"),
+    k8s.V1EnvVar(name='DATE', value="{{ dag_run.conf['date'] }}"),
+    k8s.V1EnvVar(name='ENTITIES', value="{{ dag_run.conf['entities'] }}")
+]
 
-merge_files >> rename_and_delete_file
+secrets = [
+    Secret(
+        deploy_type='env',
+        deploy_target='AWS_ACCESS_KEY_ID',
+        secret='indexer-aws-key',
+        key='aws_access_key_id'
+    ),
+    Secret(
+        deploy_type='env',
+        deploy_target='AWS_SECRET_ACCESS_KEY',
+        secret='indexer-aws-key',
+        key='aws_secret_access_key'
+    ),
+]
+
+rename_merged_object = KubernetesPodOperator(
+            image='tuannm106/merge-hour-partition:latest',
+            cmds=["main"],
+            env_vars=env_vars,
+            secrets=secrets,
+            name='rename_merged_object',
+            task_id='rename_merged_object',
+            retries=5,
+            retry_delay=timedelta(minutes=5),
+            dag=dag,
+        )
+
+merge_files >> rename_merged_object
