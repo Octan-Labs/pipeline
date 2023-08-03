b9d38df3a4bb0cb553734a5e2779e9b66ccd8dfc
Truong Ma Phi <trmaphi@gmail.com>
2023-08-03 18:08:50 +0700

    fix: error on date of dags

--- dags/bsc_daily_indexing_cronjob.py
+++ dags/bsc_daily_indexing_cronjob.py
@@ -8,7 +8,7 @@ from kubernetes.client import models as k8s
 default_args = {
     'owner': 'airflow',
     'depends_on_past': True,
-    'start_date': datetime(2020, 8, 29),
+    'start_date': datetime(2020, 8, 30),
     'retries': 1,
     'retry_delay': timedelta(minutes=30),
     'depends_on_past': False,
--- dags/bsc_pre_tx_volume_uaw_daily_cronjob.py
+++ dags/bsc_pre_tx_volume_uaw_daily_cronjob.py
@@ -7,7 +7,7 @@ from airflow.sensors.external_task import ExternalTaskSensor
 default_args = {
     'owner': 'airflow',
     'depends_on_past': True,
-    'start_date': datetime(2020, 8, 29),
+    'start_date': datetime(2020, 8, 30),
     'retries': 5,
     'retry_delay': timedelta(minutes=30),
     'depends_on_past': False,
