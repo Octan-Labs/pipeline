4ad653a99f45bb2bb0829d9727c03f4c4647a536
noobmdev <noobmdev@gmail.com>
2023-08-04 15:39:16 +0700

    update use airflow connection

--- dags/example_clickhouse_operator.py
+++ dags/example_clickhouse_operator.py
@@ -1,11 +1,19 @@
 from airflow import DAG
 from airflow_clickhouse_plugin.operators.clickhouse_operator import ClickHouseOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': True,
+    'start_date': datetime(2023, 8, 4),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=30),
+}
 
 with DAG(
         dag_id='example_clickhouse_operator',
-        'start_date': datetime(2023, 8, 4),
+        default_args=default_args
 ) as dag:
     ClickHouseOperator(
         task_id='test_select_clickhouse',
