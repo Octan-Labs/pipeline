4ccd47f32f4bd93ccd777ef3719009d51e8cecbf
noobmdev <noobmdev@gmail.com>
2023-08-04 16:35:57 +0700

    add airflow clickhouse plugin

---
+++ dags/example_clickhouse_operator.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow_clickhouse_plugin.operators.clickhouse_operator import ClickHouseOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': True,
+    'start_date': datetime(2023, 8, 4),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=15),
+}
+
+with DAG(
+        dag_id='example_clickhouse_operator',
+        default_args=default_args
+) as dag:
+    ClickHouseOperator(
+        task_id='test_select_clickhouse',
+        database='system',
+        sql=(
+            '''
+              SELECT * from processes
+            '''
+            # result of the last query is pushed to XCom
+        ),
+        clickhouse_conn_id="clickhouse_conn"
+    )
