ffb21d8c126fb21fd75989436ca713f358afdfef
noobmdev <noobmdev@gmail.com>
2023-08-04 15:47:50 +0700

    update use airflow connection

--- dags/example_clickhouse_operator.py
+++ dags/example_clickhouse_operator.py
@@ -8,7 +8,7 @@ default_args = {
     'depends_on_past': True,
     'start_date': datetime(2023, 8, 4),
     'retries': 1,
-    'retry_delay': timedelta(minutes=30),
+    'retry_delay': timedelta(seconds=15),
 }
 
 with DAG(
