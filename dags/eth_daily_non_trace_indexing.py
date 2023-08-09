f9e36fa17361c863864ce8e32758b1f7e4a5ece6
noobmdev <noobmdev@gmail.com>
2023-08-09 11:17:04 +0700

    chore: use fixed image indexer fix schema invalid

--- dags/eth_daily_non_trace_indexing.py
+++ dags/eth_daily_non_trace_indexing.py
@@ -64,7 +64,7 @@ with DAG(
     ]
 
     eth_daily_non_trace_indexing_cronjob = KubernetesPodOperator(
-                image='octanlabs/ethereumetl:0.0.4',
+                image='octanlabs/ethereumetl:0.0.5',
                 arguments=['export_all'],
                 env_vars=env_vars,
                 secrets=secrets,
