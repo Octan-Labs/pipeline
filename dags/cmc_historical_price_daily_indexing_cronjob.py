0ff1dcdf90e74bf554d755855b21797d2dfb940b
Truong Ma Phi <trmaphi@gmail.com>
2023-08-12 19:29:49 +0700

    fix(dag): merge import clickhouse to indexing job

--- dags/cmc_historical_price_daily_indexing_cronjob.py
+++ dags/cmc_historical_price_daily_indexing_cronjob.py
@@ -1,7 +1,9 @@
 from airflow import DAG
+from airflow_clickhouse_plugin.operators.clickhouse_operator import ClickHouseOperator
 from airflow.kubernetes.secret import Secret
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime, timedelta
+from airflow.models import Variable
 
 from kubernetes.client import models as k8s
 
@@ -71,4 +73,35 @@ cmc_historical_price_daily_indexing_cronjob = KubernetesPodOperator(
             dag=dag,
         )
 
-cmc_historical_price_daily_indexing_cronjob
+    base_s3_url = Variable.get("cmc_s3_url")
+
+    access_key = Variable.get("s3_access_secret_key")
+    secret_key = Variable.get("s3_secret_key")
+
+    import_from_s3_to_clickhouse = ClickHouseOperator(
+        task_id='import_from_s3_to_clickhouse',
+        database='default',
+        sql=(
+            '''
+                INSERT INTO cmc_historical
+                SELECT *
+                FROM
+                s3(
+                '{base_s3_url}/cmc_historicals/{date}.csv',
+                '{access_key}', 
+                '{secret_key}', 
+                'CSV'
+                )
+                SETTINGS parallel_distributed_insert_select=1, async_insert=1, wait_for_async_insert=1,
+                max_threads=4, max_insert_threads=4, input_format_parallel_parsing=0;
+            '''.format(
+                    base_s3_url = base_s3_url, 
+                    access_key = access_key, 
+                    secret_key = secret_key,
+                    date = "{{ data_interval_start.subtract(days=1) | ds }}"
+                )
+        ),
+        clickhouse_conn_id="clickhouse_conn"
+    )
+
+    cmc_historical_price_daily_indexing_cronjob >> import_from_s3_to_clickhouse
