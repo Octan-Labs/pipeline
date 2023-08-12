d643420712e3c4e069f3c183758f9b5e656d05ef
Truong Ma Phi <trmaphi@gmail.com>
2023-08-12 19:27:46 +0700

    fix(dag): merge import clickhouse to indexing job

--- dags/example_clickhouse_insert.py
+++ dags/example_clickhouse_insert.py
@@ -1,44 +1,44 @@
-from airflow import DAG
-from airflow_clickhouse_plugin.operators.clickhouse_operator import ClickHouseOperator
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
+# from airflow import DAG
+# from airflow_clickhouse_plugin.operators.clickhouse_operator import ClickHouseOperator
+# from airflow.operators.python_operator import PythonOperator
+# from datetime import datetime, timedelta
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 8, 4),
-    'retries': 1,
-    'retry_delay': timedelta(seconds=15)
-}
+# default_args = {
+#     'owner': 'airflow',
+#     'depends_on_past': False,
+#     'start_date': datetime(2023, 8, 4),
+#     'retries': 1,
+#     'retry_delay': timedelta(seconds=15)
+# }
 
-with DAG(
-        dag_id='example_clickhouse_insert',
-        default_args=default_args,
-        schedule_interval=None,
-        catchup=False,
-        tags=['example', 'clickhouse']
-) as dag:
-    ClickHouseOperator(
-        task_id='test_insert_clickhouse',
-        database='default',
-        sql=(
-            '''
-                INSERT INTO my_first_table (user_id, message, timestamp, metric) VALUES
-                    (101, 'Hello, ClickHouse!',                                 now(),       -1.0    ),
-                    (102, 'Insert a lot of rows per batch',                     yesterday(), 1.41421 ),
-                    (102, 'Sort your data based on your commonly-used queries', today(),     2.718   ),
-                    (101, 'Granules are the smallest chunks of data read',      now() + 5,   3.14159 )
-            ''','''
-                 SELECT * FROM my_first_table ORDER BY timestamp LIMIT 1000
-            '''
+# with DAG(
+#         dag_id='example_clickhouse_insert',
+#         default_args=default_args,
+#         schedule_interval=None,
+#         catchup=False,
+#         tags=['example', 'clickhouse']
+# ) as dag:
+#     ClickHouseOperator(
+#         task_id='test_insert_clickhouse',
+#         database='default',
+#         sql=(
+#             '''
+#                 INSERT INTO my_first_table (user_id, message, timestamp, metric) VALUES
+#                     (101, 'Hello, ClickHouse!',                                 now(),       -1.0    ),
+#                     (102, 'Insert a lot of rows per batch',                     yesterday(), 1.41421 ),
+#                     (102, 'Sort your data based on your commonly-used queries', today(),     2.718   ),
+#                     (101, 'Granules are the smallest chunks of data read',      now() + 5,   3.14159 )
+#             ''','''
+#                  SELECT * FROM my_first_table ORDER BY timestamp LIMIT 1000
+#             '''
 
-            # result of the last query is pushed to XCom
-        ),
-        clickhouse_conn_id="clickhouse_conn"
-    ) >> PythonOperator(
-        task_id='print_result',
-        provide_context=True,
-        python_callable=lambda task_instance, **_:
-            # pulling XCom value and printing it
-            print(task_instance.xcom_pull(task_ids='test_insert_clickhouse')),
-    )
+#             # result of the last query is pushed to XCom
+#         ),
+#         clickhouse_conn_id="clickhouse_conn"
+#     ) >> PythonOperator(
+#         task_id='print_result',
+#         provide_context=True,
+#         python_callable=lambda task_instance, **_:
+#             # pulling XCom value and printing it
+#             print(task_instance.xcom_pull(task_ids='test_insert_clickhouse')),
+#     )
--- dags/example_parallel_task.py
+++ dags/example_parallel_task.py
@@ -1,28 +1,28 @@
-import time
-from datetime import datetime
-from airflow.models import DAG
-from airflow.operators.python_operator import PythonOperator
+# import time
+# from datetime import datetime
+# from airflow.models import DAG
+# from airflow.operators.python_operator import PythonOperator
 
-def test(**context):
-    time.sleep(100)
+# def test(**context):
+#     time.sleep(100)
 
-default_args = {
-    "owner": 'Airflow',
-    "start_date": datetime(2021, 1, 1),
-}
+# default_args = {
+#     "owner": 'Airflow',
+#     "start_date": datetime(2021, 1, 1),
+# }
 
-dag = DAG(
-    dag_id='example_parrallel_task',
-    schedule_interval="0 * * * *",
-    default_args=default_args,
-    catchup=False,
-    tags=['example']
-)
+# dag = DAG(
+#     dag_id='example_parrallel_task',
+#     schedule_interval="0 * * * *",
+#     default_args=default_args,
+#     catchup=False,
+#     tags=['example']
+# )
 
-with dag:
-    for i in range(24):
-        task = PythonOperator(
-            task_id=f"task_{i}",
-            python_callable=test,
-            provide_context=True
-        )
+# with dag:
+#     for i in range(24):
+#         task = PythonOperator(
+#             task_id=f"task_{i}",
+#             python_callable=test,
+#             provide_context=True
+#         )
