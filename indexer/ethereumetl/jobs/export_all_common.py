f1b33cf4aac28330d63e8ffda737d22e22311ce6
tuannm <nminh.tuan106@gmail.com>
2023-08-31 12:31:04 +0700

    feat: support Clickhouse as export_all target

--- indexer/ethereumetl/jobs/export_all_common.py
+++ indexer/ethereumetl/jobs/export_all_common.py
@@ -49,82 +49,110 @@ def export_all_common(partitions, output_dir, provider_uri, max_workers, batch_s
             padded_batch_start_block=padded_batch_start_block,
             padded_batch_end_block=padded_batch_end_block,
         )
-        
-        file_name_suffix = '{padded_batch_start_block}_{padded_batch_end_block}'.format(
-            padded_batch_start_block=padded_batch_start_block,
-            padded_batch_end_block=padded_batch_end_block,
-        )
 
-        all_file_mapping = {
-            EntityType.BLOCK: '{output_dir}/blocks{partition_dir}/blocks_{file_name_suffix}'.format(
-                output_dir=output_dir,
-                partition_dir=partition_dir,
-                file_name_suffix=file_name_suffix,
-            ),
-            EntityType.TRANSACTION: '{output_dir}/transactions{partition_dir}/transactions_{file_name_suffix}'.format(
-                output_dir=output_dir,
-                partition_dir=partition_dir,
-                file_name_suffix=file_name_suffix,
-            ),
-            EntityType.LOG: '{output_dir}/logs{partition_dir}/logs_{file_name_suffix}'.format(
-                output_dir=output_dir,
-                partition_dir=partition_dir,
-                file_name_suffix=file_name_suffix,
-            ),
-            EntityType.TOKEN_TRANSFER: '{output_dir}/token_transfers{partition_dir}/token_transfers_{file_name_suffix}'.format(
-                output_dir=output_dir,
-                partition_dir=partition_dir,
-                file_name_suffix=file_name_suffix,
-            ),
-            EntityType.TRACE: '{output_dir}/traces{partition_dir}/traces_{file_name_suffix}'.format(
-                output_dir=output_dir,
-                partition_dir=partition_dir,
-                file_name_suffix=file_name_suffix,
-            ),
-            EntityType.CONTRACT: '{output_dir}/contracts{partition_dir}/contracts_{file_name_suffix}'.format(
-                output_dir=output_dir,
-                partition_dir=partition_dir,
-                file_name_suffix=file_name_suffix,
-            ),
-            EntityType.TOKEN: '{output_dir}/tokens{partition_dir}/tokens_{file_name_suffix}'.format(
-                output_dir=output_dir,
-                partition_dir=partition_dir,
-                file_name_suffix=file_name_suffix,
-            ),
-        }
+        if output_dir.startswith('clickhouse://'):
+            from blockchainetl.jobs.exporters.clickhouse_item_exporter import ClickHouseItemExporter
+            from blockchainetl.jobs.exporters.converters.decimal_to_int_item_converter import DecimalToIntItemConverter
+            item_exporter = ClickHouseItemExporter(output_dir, item_type_to_table_mapping={
+                'block': 'block',
+                'transaction': 'transaction',
+                'log': 'log',
+                'token_transfer': 'token_transfer',
+                'trace': 'trace',
+                'contract': 'contract',
+                'token': 'token',
+            },
+            converters=[DecimalToIntItemConverter()])
+            streamer_adapter = EthStreamerAdapter(
+                batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri, batch=True)),
+                item_exporter=item_exporter,
+                batch_size=batch_size,
+                max_workers=max_workers,
+                entity_types=entity_types
+            )
 
-        all_mapper_mapping = {
-            EntityType.BLOCK: block_mapper.EthBlockMapper,
-            EntityType.TRANSACTION: transaction_mapper.EthTransactionMapper,
-            EntityType.LOG: receipt_log_mapper.EthReceiptLogMapper,
-            EntityType.TOKEN_TRANSFER: token_transfer_mapper.EthTokenTransferMapper,
-            EntityType.TRACE: trace_mapper.EthTraceMapper,
-            EntityType.CONTRACT: contract_mapper.EthContractMapper,
-            EntityType.TOKEN: token_mapper.EthTokenMapper,
-        }
+            streamer_adapter.open()
+            for start_block in range(batch_start_block, batch_end_block, batch_size):
+                end_block = start_block + batch_size - 1
+                end_block = end_block if end_block < batch_end_block else batch_end_block
+                streamer_adapter.export_all(start_block, end_block)
+            streamer_adapter.close()
+        else:        
+            file_name_suffix = '{padded_batch_start_block}_{padded_batch_end_block}'.format(
+                padded_batch_start_block=padded_batch_start_block,
+                padded_batch_end_block=padded_batch_end_block,
+            )
 
-        filename_without_ext_mapping = {}
-        mapper_mapping = {}
+            all_file_mapping = {
+                EntityType.BLOCK: '{output_dir}/blocks{partition_dir}/blocks_{file_name_suffix}'.format(
+                    output_dir=output_dir,
+                    partition_dir=partition_dir,
+                    file_name_suffix=file_name_suffix,
+                ),
+                EntityType.TRANSACTION: '{output_dir}/transactions{partition_dir}/transactions_{file_name_suffix}'.format(
+                    output_dir=output_dir,
+                    partition_dir=partition_dir,
+                    file_name_suffix=file_name_suffix,
+                ),
+                EntityType.LOG: '{output_dir}/logs{partition_dir}/logs_{file_name_suffix}'.format(
+                    output_dir=output_dir,
+                    partition_dir=partition_dir,
+                    file_name_suffix=file_name_suffix,
+                ),
+                EntityType.TOKEN_TRANSFER: '{output_dir}/token_transfers{partition_dir}/token_transfers_{file_name_suffix}'.format(
+                    output_dir=output_dir,
+                    partition_dir=partition_dir,
+                    file_name_suffix=file_name_suffix,
+                ),
+                EntityType.TRACE: '{output_dir}/traces{partition_dir}/traces_{file_name_suffix}'.format(
+                    output_dir=output_dir,
+                    partition_dir=partition_dir,
+                    file_name_suffix=file_name_suffix,
+                ),
+                EntityType.CONTRACT: '{output_dir}/contracts{partition_dir}/contracts_{file_name_suffix}'.format(
+                    output_dir=output_dir,
+                    partition_dir=partition_dir,
+                    file_name_suffix=file_name_suffix,
+                ),
+                EntityType.TOKEN: '{output_dir}/tokens{partition_dir}/tokens_{file_name_suffix}'.format(
+                    output_dir=output_dir,
+                    partition_dir=partition_dir,
+                    file_name_suffix=file_name_suffix,
+                ),
+            }
 
-        for entity_type in entity_types:
-            filename_without_ext_mapping[entity_type] = all_file_mapping[entity_type]
-            mapper_mapping[entity_type] = all_mapper_mapping[entity_type]
+            all_mapper_mapping = {
+                EntityType.BLOCK: block_mapper.EthBlockMapper,
+                EntityType.TRANSACTION: transaction_mapper.EthTransactionMapper,
+                EntityType.LOG: receipt_log_mapper.EthReceiptLogMapper,
+                EntityType.TOKEN_TRANSFER: token_transfer_mapper.EthTokenTransferMapper,
+                EntityType.TRACE: trace_mapper.EthTraceMapper,
+                EntityType.CONTRACT: contract_mapper.EthContractMapper,
+                EntityType.TOKEN: token_mapper.EthTokenMapper,
+            }
 
-        streamer_adapter = EthStreamerAdapter(
-            batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri, batch=True)),
-            batch_size=batch_size,
-            item_exporter=FsspecItemExporter(
-                filename_without_ext_mapping=filename_without_ext_mapping,
-                mapper_mapping=mapper_mapping
-            ),
-            max_workers=max_workers,
-            entity_types=tuple(entity_types),
-            calculate_offset=False,
-        )
+            filename_without_ext_mapping = {}
+            mapper_mapping = {}
 
-        streamer_adapter.open()
-        streamer_adapter.export_all(batch_start_block, batch_end_block)
-        streamer_adapter.close()
+            for entity_type in entity_types:
+                filename_without_ext_mapping[entity_type] = all_file_mapping[entity_type]
+                mapper_mapping[entity_type] = all_mapper_mapping[entity_type]
+
+            streamer_adapter = EthStreamerAdapter(
+                batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri, batch=True)),
+                batch_size=batch_size,
+                item_exporter=FsspecItemExporter(
+                    filename_without_ext_mapping=filename_without_ext_mapping,
+                    mapper_mapping=mapper_mapping
+                ),
+                max_workers=max_workers,
+                entity_types=tuple(entity_types),
+                calculate_offset=False,
+            )
+
+            streamer_adapter.open()
+            streamer_adapter.export_all(batch_start_block, batch_end_block)
+            streamer_adapter.close()
         
         # # # finish # # #
         end_time = time()
