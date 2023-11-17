013eea540c6d8cb9a25acfc9728f38ca1b6b9000
Tuan Nguyen Minh <68626789+tuannm1006@users.noreply.github.com>
2023-11-17 11:36:52 +0700

    update(dag): polygon indexer (#235)

--- dags/polygon_daily_non_trace_indexing.py
+++ dags/polygon_daily_non_trace_indexing.py
@@ -1,14 +1,14 @@
 from airflow import DAG
+from airflow.models import Variable
+from airflow_clickhouse_plugin.operators.clickhouse_operator import ClickHouseOperator
 from airflow.kubernetes.secret import Secret
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime, timedelta
-
 from kubernetes.client import models as k8s
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2020, 5, 31),
-    'retries': 1,
+    'start_date': datetime(2015, 7, 31),
     'retry_delay': timedelta(minutes=30),
     'depends_on_past': False,
     # KubernetesPodOperator Defaults
@@ -21,15 +21,23 @@ default_args = {
 with DAG(
     "polygon_daily_non_trace_indexing",
     default_args=default_args,
-    description='Run polygon non trace indexer daily',
+    description='Run polygon indexer daily',
     schedule="10 0 * * *",
     catchup=False,
     max_active_runs=1,
-    concurrency=1,
     tags=['polygon']
 ) as dag:
 
-    default_polygon_indexer_secrets = [
+    env_vars = [
+        k8s.V1EnvVar(
+            name='START', value="{{ data_interval_start | ds }}"),
+        k8s.V1EnvVar(
+            name='END', value="{{ data_interval_start | ds }}"),
+        k8s.V1EnvVar(name='PARTITION_TO_HOUR', value='false'),
+        k8s.V1EnvVar(name='ENTITY_TYPES',
+                     value='block, transaction, log')
+    ]
+    secrets = [
         Secret(
             deploy_type='env',
             deploy_target='PROVIDER_URI',
@@ -41,10 +49,7 @@ with DAG(
             deploy_target='OUTPUT_DIR',
             secret='polygon-indexer-secret',
             key='output-dir'
-        )
-    ]
-
-    indexer_aws_secrets = [
+        ),
         Secret(
             deploy_type='env',
             deploy_target='AWS_ACCESS_KEY_ID',
@@ -59,58 +64,35 @@ with DAG(
         ),
     ]
 
-    spark_aws_secrets = [
-        Secret(
-            deploy_type='env',
-            deploy_target='AWS_ACCESS_KEY_ID',
-            secret='spark-aws-key',
-            key='aws_access_key_id'
+    polygon_block, polygon_transaction, polygon_log = Variable.get('polygon_block_table_name'), Variable.get(
+        'polygon_transaction_table_name'), Variable.get('polygon_log_table_name')
+
+    KubernetesPodOperator(
+        image='octanlabs/ethereumetl:0.0.13',
+        arguments=['export_all'],
+        env_vars=env_vars,
+        secrets=secrets,
+        container_resources=k8s.V1ResourceRequirements(),
+        name='polygon_non_trace_index',
+        task_id='polygon_non_trace_index',
+        random_name_suffix=True,
+    ) >> [
+        ClickHouseOperator(
+            task_id='optimize_polygon_block',
+            database='default',
+            sql=(f'OPTIMIZE TABLE {polygon_block} FINAL DEDUPLICATE SETTINGS alter_sync = 0, optimize_skip_merged_partitions = 1'),
+            clickhouse_conn_id="clickhouse_conn"
         ),
-        Secret(
-            deploy_type='env',
-            deploy_target='AWS_SECRET_ACCESS_KEY',
-            secret='spark-aws-key',
-            key='aws_secret_access_key'
+        ClickHouseOperator(
+            task_id='optimize_polygon_transaction',
+            database='default',
+            sql=(f'OPTIMIZE TABLE {polygon_transaction} FINAL DEDUPLICATE SETTINGS alter_sync = 0, optimize_skip_merged_partitions = 1'),
+            clickhouse_conn_id="clickhouse_conn"
         ),
-    ]
-
-    default_merge_task_secrets = [
-        Secret(
-            deploy_type='env',
-            deploy_target='BASE_PATH',
-            secret='polygon-indexer-secret',
-            key='output-dir'
-        ),
-    ]
-
-    polygon_daily_non_trace_indexing_tasks = []
-
-    for hour in range(24):
-        env_vars = [
-            k8s.V1EnvVar(
-                name='START',
-                value="{{ data_interval_start | ds }}"),
-            k8s.V1EnvVar(
-                name='END',
-                value="{{ data_interval_start | ds }}"),
-            k8s.V1EnvVar(
-                name='JOB_COMPLETION_INDEX',
-                value=str(hour)),
-            k8s.V1EnvVar(
-                name='ENTITY_TYPES',
-                value='block, transaction, log, token_transfer')
-        ]
-
-        KubernetesPodOperator(
-            image='octanlabs/ethereumetl:0.0.12',
-            arguments=['export_all'],
-            env_vars=env_vars,
-            secrets=default_polygon_indexer_secrets + indexer_aws_secrets,
-            container_resources=k8s.V1ResourceRequirements(
-                requests={
-                    'memory': '8G',
-                },
-            ),
-            task_id='polygon_non_trace_index_{}'.format(hour),
-            random_name_suffix=True,
+        ClickHouseOperator(
+            task_id='optimize_polygon_log',
+            database='default',
+            sql=(f'OPTIMIZE TABLE {polygon_log} FINAL DEDUPLICATE SETTINGS alter_sync = 0, optimize_skip_merged_partitions = 1'),
+            clickhouse_conn_id="clickhouse_conn"
         )
+    ]
